30b9255ca65639cd9b2dd15b5e8ac278ebefb895
Rachel Isaac <rachelisaac995@gmail.com>
2022-12-19 17:44:04 +0400

    Add functionality to nav menu links

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const hamBurger = document.querySelector(".mobile-nav");
 const cellphoneNav = document.querySelector(".cellphone-nav");
-const iconHamburger = document.querySelector(".mobile-nav-icon")
+const iconHamburger = document.querySelector(".mobile-nav-icon");
+const hamburgerNavLinks = document.querySelectorAll(".nav-link");
+
+console.log(hamburgerNavLinks);
 
 function displayNav() {
   cellphoneNav.classList.toggle("active");
-  hamBurger.classList.toggle("HamBurger-hide")
+  hamBurger.classList.toggle("HamBurger-hide");
 }
 function removeNav() {
   cellphoneNav.classList.remove("active");
-  hamBurger.classList.remove("HamBurger-hide")
+  hamBurger.classList.remove("HamBurger-hide");
 }
 
 hamBurger.addEventListener("click", displayNav);
 iconHamburger.addEventListener("click", removeNav);
 
+for (let i = 0; i < hamburgerNavLinks.length; i++) {
+  hamburgerNavLinks[i].addEventListener("click", removeNav);
+}
